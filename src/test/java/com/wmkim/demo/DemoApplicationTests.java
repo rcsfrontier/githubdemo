31148e9e7f3f1554668b7af44906b8a1ca7ccb44
rcsfrontier <rcsfrontier@gmail.com>
2025-07-01 14:29:22 +0900

    make demo project initial

---
+++ src/test/java/com/wmkim/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmkim.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
